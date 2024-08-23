df77255fea3f39c45f83401aea2289febfb89c95
Lucas <lucasdamata01@hotmail.com>
2024-08-23 09:58:30 -0300

    Coleta as evoluções
    Coleta as evoluções em uma única coluna, seguindo o formato.
    evo1_id-evo1_nome-evo_1link|evo2_id ....

--- pokemon/pokemon/spiders/pokemon.py
+++ pokemon/pokemon/spiders/pokemon.py
@@ -25,6 +25,7 @@ class PokemonScrapper(scrapy.Spider):
             'tipo 1': response.css('.vitals-table > tbody > tr:nth-child(2) > td > a:nth-child(1)::text').get(),
             'tipo 2': response.css('.vitals-table > tbody > tr:nth-child(2) > td > a:nth-child(2)::text').get(),
             'abilities': self.get_abilities(response.css('.vitals-table > tbody > tr:nth-child(6)')),
+            'evolutions': self.get_evoluion(response.css('.infocard-list-evo'))
         }
 
     def get_abilities(self, response) -> str:
@@ -33,4 +34,15 @@ class PokemonScrapper(scrapy.Spider):
         for abilitie in abiliities:
             name = abilitie.css('a::text').get()
             resp = resp + name + "|"
+        return resp
+    
+    def get_evoluion(self, resposnse) -> str:
+        resp = ""
+        list_evolution = resposnse.css('div')
+        for evolution in list_evolution:
+            info = evolution.css('span.text-muted')
+            num = info.css('small::text').get()
+            nome = info.css('a::text').get()
+            link = info.css('a::attr(href)').get()
+            resp += f"{num}-{nome}-{self.domain}{link}|"
         return resp
